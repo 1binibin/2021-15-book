69a340a6d2941d3830b048a984bf9a450ba502c5
1binibin <namwonbin@gmail.com>
2021-09-02 14:37:37 +0900

    pool

--- routes/sql/index.js
+++ routes/sql/index.js
@@ -2,9 +2,23 @@ const path = require('path')
 const express = require('express')
 const router = express.Router()
 const { error } = require('../../modules/util')
+const { pool } = require('../../modules/mysql.js')
 
-router.use((req, res, next) => {
 
+
+router.get('/list', async (req, res, next) => {
+	let sql = 'SELECT * FROM books'
+	let r = await pool.execute (sql)
+		res.status(200).json(r)
+})
+
+router.get('/create', async(req, res, next) => {
+	let title = '홍길동전'
+	let writer = '허균'
+	let content = '아버지를 아버지라...'
+	let sql = `INSERT INTO books SET title='${title}', writer='${writer}', content='${content}' `
+	let r = await pool.query (sql)
+		res.status(200).json(r)
 })
 
 
