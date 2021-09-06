179b44f1667aa22c4ea963cb55b565fe38828925
1binibin <namwonbin@gmail.com>
2021-09-06 11:14:45 +0900

    dbf

--- routes/book/index.js
+++ routes/book/index.js
@@ -6,11 +6,13 @@ const { error } = require('../../modules/util')
 const saveRouter = require('./save-router')
 const formRouter = require('./form-router')
 const listRouter = require('./list-router')
+const viewRouter = require('./view-router')
 
 
 router.use('/form', formRouter)			// HTML: 글작성페이지
 router.use('/save', saveRouter)			// POST: 저장
 router.use('/list', listRouter)			// HTML / GET: 리스트페이지
+router.use('/view', viewRouter)			//
 
 
 module.exports = router
