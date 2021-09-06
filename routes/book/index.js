46ddcf764668a5ae3ac890dff7108b5857909ea3
1binibin <namwonbin@gmail.com>
2021-09-06 14:55:29 +0900

    CRUD 구현

--- routes/book/index.js
+++ routes/book/index.js
@@ -3,27 +3,28 @@ const express = require('express')
 const router = express.Router()
 const { error } = require('../../modules/util')
 
-const saveRouter = require('./save-router')
 const formRouter = require('./form-router')
 const listRouter = require('./list-router')
 const viewRouter = require('./view-router')
+const createRouter = require('./create-router')
 const deleteRouter = require('./delete-router')
+const updateRouter = require('./update-router')
 
-
-router.post('/', saveRouter)			// POST: 저장
-router.delete('/', viewRouter)		// DELETE: 삭제
-router.use('/form', formRouter)		// HTML: 글작성페이지
-router.get('/', listRouter)				// HTML / GET: 리스트페이지
-router.get('/:idx', viewRouter)		// HTML / GET: 상세페이지
-
+router.post('/', createRouter)			// POST: 저장
+router.put('/', updateRouter)				// UPDATE: 수정
+router.delete('/', deleteRouter)		// DELETE: 삭제
+router.use('/form', formRouter)			// HTML: 글작성(수정)페이지
+router.use('/view', viewRouter)			// HTML/GET: 상세페이지
+router.use('/', listRouter)	// HTML/GET: 리스트페이지
 
 module.exports = router
 
 /* 
-GET  		/book    					전체리스트 LIST
-GET  		/book?page=1    	페이지전체리스트 LIST
-GET  		/book/1	  				상세리스트VIEW
-POST 		/book     				저장 save
-PUT  		/book     				업데이트 update
-DELETE  /book     				삭제 delete
+GET  		/book,/book/list		페이지리스트 LIST - 1page
+GET  		/book/list/:page		페이지리스트 LIST - 해당페이지
+GET  		/book/view/1				상세리스트 VIEW
+GET  		/book/update/1			상세리스트 UPDATE
+POST 		/book								저장 save
+PUT  		/book								업데이트 update
+DELETE 	/book								삭제 delete
 */
