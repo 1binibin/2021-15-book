b4e2eeb08d67f901b4d133026135ebc931474482
1binibin <namwonbin@gmail.com>
2021-09-15 10:22:13 +0900

    sql문 수정,

--- routes/book/save-router.js
+++ routes/book/save-router.js
@@ -8,14 +8,15 @@ router.post('/', uploader.fields( [ {name: 'cover'}, {name: 'upfile'} ] ), async
 	let sql, values
 	try {
 		const { title, writer, content, _method, idx } = req.body
-		sql = ( _method === 'PUT' && idx ) ? 'UPDATE books' : 'INSERT INTO books'
+		const isUpdate = ( _method === 'PUT' && idx )
+		sql = isUpdate ? 'UPDATE books' : 'INSERT INTO books'
 		sql += ' SET title=?, writer=?, content=? '
-		sql += ( _method === 'PUT' && idx ) ? 'WHERE idx='+idx : ''
+		sql += isUpdate ? 'WHERE idx='+idx : ''
 		values = [title, writer, content]
 
 		const [rs]= await pool.execute(sql, values)
 
-		if(req.files) { // 첨부파일이 있다면
+		/* if(req.files) { // 첨부파일이 있다면
 			for(let [k, [v]] of Object.entries(req.files)) {
 				let { originalname, filename,mimetype, size } = v
 				sql = ( _method === 'PUT' && idx ) ? 'UPDATE files ' : 'INSERT INTO files '
@@ -23,7 +24,7 @@ router.post('/', uploader.fields( [ {name: 'cover'}, {name: 'upfile'} ] ), async
 				values = [rs.insertId, originalname, filename, mimetype, size, k.substr(0, 1).toUpperCase()]
 				await pool.execute(sql, values)
 			}
-		}
+		} */
 	res.redirect(`/${req.lang}/book`)
 	}
 	catch (err){
