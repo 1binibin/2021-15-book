a5aad36ecb837b55d87c45703882be9a69769b8c
1binibin <namwonbin@gmail.com>
2021-09-15 12:22:57 +0900

    파일이동 간 수정 sql문 알아보기

--- routes/book/save-router.js
+++ routes/book/save-router.js
@@ -9,22 +9,33 @@ router.post('/', uploader.fields( [ {name: 'cover'}, {name: 'upfile'} ] ), async
 	try {
 		const { title, writer, content, _method, idx } = req.body
 		const isUpdate = ( _method === 'PUT' && idx )
-		sql = isUpdate ? 'UPDATE books' : 'INSERT INTO books'
-		sql += ' SET title=?, writer=?, content=? '
-		sql += isUpdate ? 'WHERE idx='+idx : ''
+		sql = isUpdate ? " UPDATE books " : " INSERT INTO books "
+		sql += " SET title=?, writer=?, content=? "
+		sql += isUpdate ? " WHERE idx= " + idx : ""
 		values = [title, writer, content]
 
 		const [rs]= await pool.execute(sql, values)
+		// console.log(sql)
+		// console.log(rs)
+		/* 
+		1. 위의 Query가 INSERT라면 rs = { insertId: 15, affectedRows: 1 ... }
+		2. 위의 Query가 UPDATE라면 rs = { affectedRows: 1 ...}
+		3. files Query는 아래와 같다.
+			INSERT INTO files SET oriname=?, savename=?, mimetype=?, size=?, fieldname=?, fidx=?
+			- fidx 어디서 ? => rs.insertId
+			UPDATE files SET oriname=?, savename=?, mimetype=?, size=?, fieldname=?, status=? WHERE idx=?
+			- idx= 어디서 ? => SELECT * FROM files WHERE fidx = idx
+		*/
 
-		/* if(req.files) { // 첨부파일이 있다면
+		 /* if(req.files) { // 첨부파일이 있다면
 			for(let [k, [v]] of Object.entries(req.files)) {
 				let { originalname, filename,mimetype, size } = v
-				sql = ( _method === 'PUT' && idx ) ? 'UPDATE files ' : 'INSERT INTO files '
-				sql += ' oriname=?, savename=?, mimetype=?, size=?, fieldname=? '
+				sql = ( _method === 'PUT' && idx ) ? " UPDATE files " : " INSERT INTO files "
+				sql += " SET oriname=?, savename=?, mimetype=?, size=?, fieldname=? "
 				values = [rs.insertId, originalname, filename, mimetype, size, k.substr(0, 1).toUpperCase()]
 				await pool.execute(sql, values)
 			}
-		} */
+		}  */
 	res.redirect(`/${req.lang}/book`)
 	}
 	catch (err){
