071a7f6ee1c853f3c9df4797c503eb4b4a768c2e
1binibin <namwonbin@gmail.com>
2021-09-24 10:28:43 +0900

    인젝션 공격 방어 해보자

--- 03.crypto.js
+++ 03.crypto.js
@@ -19,8 +19,8 @@ let pass512 = crypto.createHash('sha512').update(pass + salt).digest('base64')
 let pass2 = '123456'
 let pass512Re = crypto.createHash('sha512').update(pass2 + salt).digest('base64')
 
-if(pass512 === pass512Re) console.log('로그인 되었습니다.')
-else console.log('패스워드가 틀렸습니다')
+// if(pass512 === pass512Re) console.log('로그인 되었습니다.')
+// else console.log('패스워드가 틀렸습니다')
 
 const genPass = async pass => { // 암호화
 	return await bcrypt.hash(pass + salt, 5)
@@ -34,9 +34,9 @@ const comparePass = async (pass, hash) => { // 검증
 const passVerify = async () => {
 	let pass = '123456'
 	let hash = await genPass(pass)
-	console.log(hash)
+	// console.log(hash)
 	let compare = await comparePass('123456', hash)
-	console.log(compare)
+	// console.log(compare)
 }
 
 passVerify();
@@ -46,15 +46,35 @@ const passVerity2 = async () => {
 	let salt = 'jasdf9)a'
 	let hash = await bcrypt.hash( pass + salt, 9 )
 	let compare = await bcrypt.compare( pass + salt , hash )
-	console.log("verify2: ", compare)
+	// console.log("verify2: ", compare)
 }
 
 passVerity2()
 
 // 암복호화 예제
 const encrypt = cipher.AES.encrypt('아버지를 아버지', salt).toString()
-console.log("encrypt: ", encrypt)
+// console.log("encrypt: ", encrypt)
 
 const decrypt = cipher.AES.decrypt(encrypt, salt)
 const text = decrypt.toString(cipher.enc.Utf8)
-console.log("decrypt: ", text)
+// console.log("decrypt: ", text)
+
+/* let password = '123456'
+let password2 = 'abcdef'
+const base64 = password => {
+	crypto.randomBytes(64, (err, buf) => {
+		const salt = buf.toString('base64')
+		crypto.pbkdf2(password, salt, 100, 64, 'sha512', (err, key) =>{
+			console.log(key.toString('base64'))
+		})
+	}) 
+}*/
+
+// base64(password)
+// base64(password2)
+
+/*const genPass2  = async pass =>{
+	return await bcrypt.hash(pass + salt, 5)
+}
+
+genPass('123456').then((r) => console.log(r) ) */
