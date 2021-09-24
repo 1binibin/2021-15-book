10cfde3a5c7e168229dcb05123d861a4105d2c18
1binibin <namwonbin@gmail.com>
2021-09-24 11:22:52 +0900

    인젝션 방어, 아이디 중복 체크, 오류 전달

--- routes/api/auth/verify-router.js
+++ routes/api/auth/verify-router.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const router = express.Router()
 const { error } = require('../../../modules/util')
-const { pool } = require('../../../modules/mysql-init')
+const { mysql, pool } = require('../../../modules/mysql-init')
 
 router.get('/verify', async (req, res, next) => {
 	// userid, email 중복 검증
@@ -11,16 +11,16 @@ router.get('/verify', async (req, res, next) => {
 	let value = req.query.value
 	try {
 		if(value.includes("'") || value.includes('"')){
-			res.status(401).json({ error: '특수문자가 포함되어 있습니다.' })
+			res.status(400).json({ msg: '특수문자가 포함되어 있습니다.' })
 		}
 		else {
-			sql = ` SELECT ${key} FROM users WHERE ${key}=? `
+			sql = ` SELECT * FROM users WHERE ${key} = ? `
 			const [ rs ] = await pool.execute(sql, [ value ])
 			res.status(200).json( { isUsed: rs.length ? true : false } )
 		}
 	} 
 	catch (err) {
-		res.status(500).json(err)
+		res.status(500).json({ msg: '서버오류 입니다. 다시 시도해 주세요.',err })
 	}
 })
 
