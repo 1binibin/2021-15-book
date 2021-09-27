c1037cd7cd4ecb441bab786bb6ccc266912e348e
1binibin <namwonbin@gmail.com>
2021-09-27 17:04:03 +0900

    권한 설정

--- routes/book/delete-router.js
+++ routes/book/delete-router.js
@@ -5,8 +5,9 @@ const router = express.Router()
 const createError = require('http-errors')
 const { moveFile } = require('../../modules/util')
 const { pool } = require('../../modules/mysql-init')
+const { isUser, isGuest, isMyBook } = require('../../middlewares/auth-mw')
 
-router.delete('/', async (req, res, next) => {
+router.delete('/', isUser, isMyBook('body'), async (req, res, next) => {
 	try {
 		// sql = "DELETE FROM books WHERE idx=?"
 		sql = "UPDATE books SET status='0' WHERE idx=" + req.body.idx
