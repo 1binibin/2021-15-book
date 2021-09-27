958184b373a779d5af7cccfeb796e707daf1bbda
1binibin <namwonbin@gmail.com>
2021-09-27 10:22:20 +0900

    login 작업 준비

--- routes/book/list-router.js
+++ routes/book/list-router.js
@@ -10,6 +10,8 @@ const createPager = require('../../modules/pager-init')
 
 router.get(['/', '/:page'], async (req, res, next) => {
 	req.app.locals.PAGE = 'LIST'
+	req.app.locals.js = 'book/list'
+	req.app.locals.css = 'book/list'
 	let sql, values;
 	try {
 		sql = "SELECT COUNT(idx) FROM books WHERE status > '0'"
@@ -37,10 +39,8 @@ router.get(['/', '/:page'], async (req, res, next) => {
 			v.cover =v.cover ? relPath(v.cover) : null
 			v.icon = v.icon ? getIcon(v.icon) : null
 		})
-		const js = 'book/list'
-		const css = 'book/list'
 		
-		res.status(200).render('book/list', {  js, css, books, pager })
+		res.status(200).render('book/list', { books, pager })
 	}
 	catch(err) {
 		next(createError(err))
