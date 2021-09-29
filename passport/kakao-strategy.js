ea7da49ebacf6a6aa0498d5947eb9e558a6d0a1c
1binibin <namwonbin@gmail.com>
2021-09-29 13:00:05 +0900

    kakao login 오류 처리 완료 - profile.image가 없는 경우 null값 처리

--- passport/kakao-strategy.js
+++ passport/kakao-strategy.js
@@ -3,24 +3,26 @@ const { createSnsUser } = require('../models/auth')
 
 const cb = async (accessToken, refreshToken, profile, done) => {
 	try {
-		console.log('=======')
-		console.log(accessToken)
-		console.log(refreshToken)
-		console.log(profile)
-		console.log('=======')
+		// console.log('==================')
+		// console.log(accessToken)
+		// console.log(refreshToken)
+		// console.log(profile)
+		// console.log('==================')
 		let user = { userid: profile.id, accessToken }
 		let userSns = {
 			accessToken, 
 			refreshToken, 
 			provider: 'KA', 
 			snsid: profile.id,
-			snsName: profile.username,
-			displayName: profile.displayName,
-			profileURL: profile._json.properties.profile_image,
-			email: profile._json.kakao_account.email,
+			snsName: profile.username || null,
+			displayName: profile.displayName || null,
+			profileURL: profile._json.properties.profile_image || null,
+			email: profile._json.kakao_account.email || null,
 		}
+		findSnsUser(user.userid)
 		const { idx } = await createSnsUser(user, userSns)
 		user.idx = idx
+		console.log(user, idx)
 		done(null, user)
 	}
 	catch(err) {
@@ -29,9 +31,9 @@ const cb = async (accessToken, refreshToken, profile, done) => {
 }
 
 const kakaoStrategy = new KakaoStrategy({
-	clientID : process.env.KAKAO_KEY,
+	clientID: process.env.KAKAO_KEY,
 	clientSecret: process.env.KAKAO_SALT,
-	callbackURL : '/auth/kakao/cb'
+	callbackURL: '/auth/kakao/cb'
 }, cb)
 
 module.exports = passport => passport.use(kakaoStrategy)
