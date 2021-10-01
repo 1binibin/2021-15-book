243663f824d0260a72938c6b0462b9478a1af859
1binibin <namwonbin@gmail.com>
2021-10-01 17:45:07 +0900

    update-user : 에러 apikey 생성 : 성공

--- back/app.js
+++ back/app.js
@@ -57,6 +57,7 @@ const apiBookRouter = require('./routes/api/book')
 const authRouter = require('./routes/auth')
 const apiAuthRouter = require('./routes/api/auth')
 const mypageRouter = require('./routes/mypage')
+const apiMypageRouter = require('./routes/api/mypage')
 
 
 app.use(langMW)
@@ -65,6 +66,7 @@ app.use('/api/book', apiBookRouter)
 app.use('/auth', authRouter)
 app.use('/api/auth', apiAuthRouter)
 app.use('/mypage', mypageRouter)
+app.use('/api/mypage', apiMypageRouter)
 
 
 /**************** error init **************/
