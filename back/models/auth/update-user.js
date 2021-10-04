b0fe16252df3b06204495d196d29955178e32f02
1binibin <namwonbin@gmail.com>
2021-10-04 10:20:51 +0900

    navi - 추가 mypage - 수정

--- back/models/auth/update-user.js
+++ back/models/auth/update-user.js
@@ -1,29 +1,23 @@
 const bcrypt = require('bcrypt')
-const createError = require('http-errors')
 const { pool } = require('../../modules/mysql-init')
 const { existUser, findUser } = require('./find-user')
 const isValid = require('./is-valid')
 
-
-const updateUser = async (idx, passwd, username, email, sql, values) => {
+const updateUser = async ({ idx, passwd, username, email, sql, values }) => {
 	try {
 		let { BCRYPT_SALT: salt, BCRYPT_ROUND: round } = process.env
 		let { user } = await findUser('idx', idx)
-		console.log('update-user.user: ',user)
 		if(!username || !email || user.status < 3) {
 			throw new Error('ERROR')
 		}
-		else if(user.status < 5){
+		else if(user.status < 5) {
 			sql = " UPDATE users SET username=?, email=? WHERE idx=?"
-			values = [ username, email, idx ]
-			console.log('values',values)
+			values = [username, email, idx]
 		}
 		else {
 			let hashPasswd = await bcrypt.hash(passwd + salt, Number(round))
-			sql = " UPDATE users SET passwd=? username=?, email=? WHERE idx=?"
+			sql = " UPDATE users SET passwd=?, username=?, email=? WHERE idx=?"
 			values = [hashPasswd, username, email, idx]
-			console.log('values',values)
-			await pool.execute(sql, values)
 		}
 		let [rs] = await pool.execute(sql, values)
 		return rs.affectedRows === 1
@@ -32,7 +26,5 @@ const updateUser = async (idx, passwd, username, email, sql, values) => {
 		throw new Error(err)
 	}
 }
-
-
 
 module.exports = { updateUser }
