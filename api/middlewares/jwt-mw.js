b899e038a1625d724b8b9e5432c19ca5ecd63174
1binibin <namwonbin@gmail.com>
2021-10-14 11:07:12 +0900

    update model error -> throw new Error

--- api/middlewares/jwt-mw.js
+++ api/middlewares/jwt-mw.js
@@ -1,7 +1,7 @@
 /**
  * 1. 요청이 들어온다.
- *  가. req.cookies의 존재 여부 확인
- *      - !존재
+    @ req.cookies의 존재 여부 확인
+        - !존재
             = DB에서 origin과 api의 일치 여부 확인
             +  일치
               . token을 발행 (jwt.sign()) - data: { userid, apikey, origin }
@@ -9,7 +9,7 @@
               . next()
             + !일치
               . next(createError(401, 'Authorization Fail'))
- *      -  존재
+        -  존재
           = req.cookies에서 token을 확인하고 `jwt.verify(token, salt): 내용리턴`
           = 리턴된 내용에서 전달받은 origin & apikey의 일치 여부 확인
             +  일치
@@ -19,12 +19,17 @@
  */
 
 const jwt = require('jsonwebtoken')
+const createError = require('http-erros')
+const { pool } = require('../modules/mysql-init')
 
 const isApiUser = async (req, res, next) => {
   try {
-    
+    const domain = req.protocol + '://' + req.headers.host
+    const apikey = req.query.apikey
+    if(rs.length === 1) next()
+    else next(createError(401, 'Authorization Fail'))
   } catch (err) {
-    
+    next(createError(err))
   }
 }
 
