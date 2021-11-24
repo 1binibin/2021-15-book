f6a5795540bc71be61026190ba983340a2a52b4e
1binibin <namwonbin@gmail.com>
2021-11-24 21:55:36 +0900

    go-view 수정

--- back/public/js/index.js
+++ back/public/js/index.js
@@ -2,13 +2,18 @@ var y = window.innerHeight;
 window.addEventListener('scroll', function () {
   if (window.scrollY >= y - 64) {
     $('.navi-wrapper').addClass('active');
+    $('#btTop').css('display', 'block');
   } else {
     $('.navi-wrapper').removeClass('active');
+    $('#btTop').css('display', 'none');
   }
 });
 
-$('#git').click(goGit);
+$('#btTop').click(function () {
+  window.scrollTo(0, 0);
+});
 
+$('#git').click(goGit);
 function goGit() {
   location.href = 'https://github.com/1binibin';
 }
