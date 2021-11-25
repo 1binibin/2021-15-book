d46705ba96ab159f634c8170a07e3726e0645dee
1binibin <namwonbin@gmail.com>
2021-11-25 15:25:27 +0900

    z-index 수정

--- back/public/js/index.js
+++ back/public/js/index.js
@@ -13,9 +13,28 @@ $('#btTop').click(function () {
   window.scrollTo(0, 0);
 });
 
-$('#git').click(goGit);
-function goGit() {
-  location.href = 'https://github.com/1binibin';
+/* var skill = document.querySelector('.skill-wrapper');
+var title = document.querySelector('.title');
+var skills = skill.offsetTop - title.offsetHeight;
+console.log(typeof title.outerHeight);
+console.log(skill); */
+var skill = $('.skill-wrapper').offset().top;
+var prj = $('.project-wrapper').offset().top;
+var foot = $('.footer-wrapper').offset().top;
+
+function goSkill() {
+  window.scrollTo(0, skill - 200);
+}
+
+function goPrj() {
+  window.scrollTo(0, prj - 170);
+}
+
+function goContact() {
+  window.scrollTo(0, foot);
+}
+function goTop() {
+  window.scrollTo(0, 0);
 }
 
 AOS.init({
